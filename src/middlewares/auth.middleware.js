cdff011dd3a44ecd3cf0645a5464ffc78dc18975
Sohail Nadaf <sohailnadaf967@gmail.com>
2024-04-25 12:25:39 +0530

    added the subscription model

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,9 +3,14 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
+//this is used to verify the token && just for reference
+//the reason to verify the token and debug is because user gets the encrypted token and the one have is in the databasse we have to match those to proceed further
+
 export const verifyJWT = asyncHandler(async (req, res, next) =>{
  try {
    const token = req.cookies?.accessToken || req.header("Authorisation")?.replace("Bearer ", "")
+
+   console.log(token);
  
    if(!token){
      throw new ApiError(401, "Unauthorised request")
