e3b4880eb7d588a049d74e66452657a591e95a26
Sohail Nadaf <sohailnadaf967@gmail.com>
2024-04-28 18:44:13 +0530

    added the routes fo all the route controllers

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,7 @@
 // routes are basically some function whenever that URL hits it it does some functions as intentded those are routes
 
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAcessToken, registerUser } from "../../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAcessToken, registerUser, updateAccountAvatar, updateAccountDetails, updateUserCoverImage } from "../../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -16,7 +16,22 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
+
 router.route("/refresh-token").post(refreshAcessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-users").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateAccountAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/:userName").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 
 
